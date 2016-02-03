9de866382ee1b657d6f96efb4581cb6bb2e2d885
Casey Weed <me@caseyweed.net>
2016-02-02 20:35:28 -0500

    added SNode and AStar pane

--- src/AStar.java
+++ src/AStar.java
@@ -1,12 +1,20 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class AStar extends Application {
     public static void main(String[] args) {
@@ -15,30 +23,55 @@ public class AStar extends Application {
 
     @Override
     public void start(Stage stage) {
-        // a-star pane
-        AStarPoint AStarPointPane = new AStarPoint();
 
-        // borderpane
+        // layouts
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(AStarPointPane);
+        HBox hbox = new HBox(10);
+        hbox.setPadding(new Insets(10));
+        hbox.setStyle("-fx-background-color: #ddd;");
+        borderPane.setBottom(hbox);
+
+        // controls
+        Button newSceneBtn = new Button("New Scenario");
+        Button replaySceneBtn = new Button("Replay Scenario");
+        Label robotSizeLbl = new Label("Robot Size:");
+        Button incRobotSize = new Button("+");
+        Button decRobotSize = new Button("-");
+        Label obstacleSizeLbl = new Label("Obstacle size:");
+        Button incObstacleSize = new Button("+");
+        Button decObstacleSize = new Button("-");
+
+        // add
+        hbox.getChildren().addAll(newSceneBtn, replaySceneBtn, robotSizeLbl,
+                incRobotSize, decRobotSize, obstacleSizeLbl, incObstacleSize, decObstacleSize);
+
+        // a star
+        AStarSimple as = new AStarSimple();
+        as.setRobot(new Polygon(0, 0, 10, 0, 10, 10, 0, 10));
+        borderPane.setCenter(as);
+
+        // buttons
+        newSceneBtn.setOnAction(e -> as.newScenario());
+        replaySceneBtn.setOnAction(e -> as.replayScenario());
+        incRobotSize.setOnAction(e -> as.incRobotSize(2));
+        decRobotSize.setOnAction(e -> as.decRobotSize(2));
+        incObstacleSize.setOnAction(e -> as.incObstacleSize(2));
+        decObstacleSize.setOnAction(e -> as.decObstacleSize(2));
 
         // scene & stage
-        final Scene scene = new Scene(borderPane, 640, 480);
-        stage.setTitle("A Star");
+        final Scene scene = new Scene(borderPane, 720, 480);
+        stage.setTitle("A* Pathfinding");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-
-        AStarPointPane.paint();
     }
 
-    // A* just using a single point and simple 'nodes'
-    static class AStarPoint extends Pane {
-        double dotSize = 2.0;
-        double stepSize = 1.0;
-        double startX, startY;
-        double endX, endY;
-        double w, h;
+    static class AStarSimple extends Pane {
+        SNode start;
+        SNode goal;
+        double robotSize = 10;
+        double obstacleSize = 10;
+        Shape robot;
         List<Shape> obstacles = new ArrayList<>();
 
         private enum Direction {
@@ -56,139 +89,51 @@ public class AStar extends Application {
             }
         }
 
-        private class SortedNodeList {
-            private List<Node> list = new ArrayList<>();
+        public void incRobotSize(double step) {
+            this.robotSize += step > 0 ? step : 1;
+        }
 
-            public SortedNodeList() {
+        public void decRobotSize(double step) {
+            this.robotSize -= step > 0 && robotSize > 2 ? step : 0;
+        }
 
-            }
+        public void incObstacleSize(double step) {
+            this.obstacleSize += step > 0 ? step : 1;
+        }
 
-            public SortedNodeList(Node... nodes) {
-                for (Node n : nodes) {
-                    add(n);
-                }
-            }
+        public void decObstacleSize(double step) {
+            this.obstacleSize -= step > 0 && obstacleSize > 2 ? step : 0;
+        }
 
-            public Node getFirst() {
-                return list.get(0);
-            }
-
-            public void clear() {
-                list.clear();
-            }
-
-            public void add(Node node) {
-                list.add(node);
-                Collections.sort(list);
-            }
-
-            public void remove(Node node) {
-                list.remove(node);
-            }
-
-            public int size() {
-                return list.size();
-            }
-
-            public boolean contains(Node node) {
-                return list.contains(node);
+        public class SNodeComparator implements Comparator<SNode> {
+            @Override
+            public int compare(SNode a, SNode b) {
+                return Double.compare(a.getF(), b.getF());
             }
         }
 
-
-        private Node getNeighborNode(Node parent, Direction d) {
-            Node child = new Node(parent.getX() + d.dx, parent.getY() + d.dy);
-            Circle childCircle = child.toCircle();
-            childCircle.setFill(Color.BLUE);
-            getChildren().add(childCircle);
-
-            // key is the obstacles list, not the shit we throw on the stage to check
-            boolean collision = false;
-            for (Shape enemy : obstacles) {
-                if (enemy != childCircle) {
-                    Shape intersection = Shape.intersect(enemy, childCircle); // creates intersection shape of the two shapes
-                    if (intersection.getBoundsInLocal().getWidth() != -1) { // if the shape has no width, no intersection!
-                        collision = true;
-                    }
-                }
-            }
-
-            getChildren().remove(childCircle);
-            child.setObstacle(collision);
-
-            return child;
+        public void setRobot(Shape shape) {
+            this.robot = shape;
         }
 
-        private LinkedList<Node> getNeighbors(Node parent) {
-            LinkedList<Node> neighbors = new LinkedList<>();
-            for (Direction d : Direction.values()) {
-                Node n = getNeighborNode(parent, d);
-                n.setF(endX, endY); // TODO: need to pass end node to stay consistent with using nodes, not raw coords
-                neighbors.add(n);
-            }
-            return neighbors;
+        public void newScenario() {
+            // draw new scenario:
+            // 1. randomize location of obstacles within middle of the pane
+            // 2. establish start/goal points on left and right side
+            // 3. immediately try to solve
+            // for when you know, it works
+        }
+
+        public void replayScenario() {
+            // for when I get animation working
         }
 
         protected void paint() {
-            // set constants
-            w = getWidth() / 4f;
-            h = getHeight() / 4f;
-
-            // start & end points
-            startX = w - (w / 2f);
-            startY = h - (h / 2f);
-            endX = w * 3 + (w / 2f);
-            endY = h * 3 + (h / 2f);
-
-            // for convenience and visualization
-            // TODO: ask for how many obstacles, randomize the types (say an octagon, convex poly, circle, etc)
-            addObstacle();
-
-            // begin find path
-            boolean done = false;
-
-            // start, goal nodes
-            Node end = new Node(endX, endY);
-            end.setF(end);
-            Node start = new Node(startX, startY, end);
-            Circle startCircle = start.toCircle();
-            Circle endCircle = end.toCircle();
-            endCircle.setFill(Color.DARKGOLDENROD);
-            getChildren().addAll(startCircle, endCircle);
-
-            // lists for searching
-            List<Node> closed = new ArrayList<>();
-            SortedNodeList open = new SortedNodeList(start);
-
-            while (!done) {
-                Node current = open.getFirst();
-
-                // check if goal node
-                if (current == end) {
-                    done = true;
-                    continue;
-                }
-
-                // move current node to closed list, we've already visited it
-                closed.add(current);
-                open.remove(current);
-
-                for (Node n : getNeighbors(current)) {
-                    // check
-                }
-
-                // TODO: temporary, remove when done
-                done = true;
-            }
-
-            // TODO: Create final path here after we've finished searching
+            // ?
         }
 
-        // TODO: Lines do not play well with intersection, possibly use Paths instead and convert to shapes?
-        private void addObstacle() {
-            Rectangle rect = new Rectangle(w, h, w * 2, h * 2);
-            obstacles.add(rect);
-            getChildren().add(rect);
+        public void solve() {
+            // do pathfinding
         }
     }
 }
---
+++ src/SNode.java
@@ -0,0 +1,103 @@
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+
+public class SNode {
+    private static final double defaultSize = 2.0;
+    private final double x, y;
+    private double f, h;
+    private static final double g = 10.0;
+    private SNode parent = null;
+    private boolean obstacle = false;
+
+    public SNode(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public SNode(Point2D pt) {
+        this.x = pt.getX();
+        this.y = pt.getY();
+    }
+
+    public SNode(double x, double y, SNode goal) {
+        this.x = x;
+        this.y = y;
+        setF(goal);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Point2D getPoint2D() {
+        return new Point2D(x, y);
+    }
+
+    public double distanceTo(SNode snode) {
+        return Math.abs(x - snode.getX())  + Math.abs(y - snode.getY());
+    }
+
+    public double distanceTo(double x, double y) {
+        return Math.abs(this.x - x)  + Math.abs(this.y - y);
+    }
+
+    public void setF(SNode goal) {
+        h = distanceTo(goal.getX(), goal.getY());
+        f = g + h;
+    }
+
+    public void setF(double x, double y) {
+        h = distanceTo(x, y);
+        f = g + h;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public void setH(SNode goal) {
+        this.h = distanceTo(goal.getX(), goal.getY());
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public double getG() {
+        return g;
+    }
+
+    public void setObstacle(boolean obstacle) {
+        this.obstacle = obstacle;
+    }
+
+    public boolean isObstacle() {
+        return obstacle;
+    }
+
+    public Circle getShape() {
+        return new Circle(x, y, defaultSize, Color.BLUE);
+    }
+
+    public Circle getShape(double size) {
+        return new Circle(x, y, size, Color.BLUE);
+    }
+
+    public boolean containedBy(Polygon p) {
+        return p.contains(getPoint2D());
+    }
+
+    public SNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SNode parent) {
+        this.parent = parent;
+    }
+}
